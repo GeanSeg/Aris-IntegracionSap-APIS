97da5972b867270c3dd969c25e085ea2fbb18ddf
AldairTerrel <aldairterrelc@gmail.com>
2025-05-31 12:40:08 -0500

    Prueba_Commit_31-05-2025

--- WSArisSap/WSArisSap/Program.cs
+++ WSArisSap/WSArisSap/Program.cs
@@ -25,8 +25,6 @@ builder.Services.AddTransient<SapResultadoPedido>();
 builder.Services.AddTransient<SapResultadoEntrega>();
 builder.Services.AddTransient<SapResultadoFactura>();
 
-
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
