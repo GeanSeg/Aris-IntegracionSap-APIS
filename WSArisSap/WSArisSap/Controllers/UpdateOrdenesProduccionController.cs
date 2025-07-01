ce668f076c8245a00f9577ec0ae37e4ef85939ab
AldairTerrel <aldairterrelc@gmail.com>
2025-07-01 15:12:29 -0500

    Corrección ZPP_FM_ACT_ESTADO_OF no retornaba las ordenes 1-07-2025

--- WSArisSap/WSArisSap/Controllers/UpdateOrdenesProduccionController.cs
+++ WSArisSap/WSArisSap/Controllers/UpdateOrdenesProduccionController.cs
@@ -27,6 +27,40 @@ namespace WSpruebaArisSap.Controllers
             _configuration = configuration;
         }
 
+        private string FormatOrden(string KnnrInput)
+        {
+            const int knnrLength = 12;
+
+            // Si el valor es nulo o vacío, devolver cadena vacía
+            if (string.IsNullOrWhiteSpace(KnnrInput))
+            {
+                return "";
+            }
+
+            // Limpiar espacios en blanco
+            string trimmedKnnr = KnnrInput.Trim();
+
+            // Verificar si el valor comienza con una letra
+            if (char.IsLetter(trimmedKnnr[0]))
+            {
+
+                return trimmedKnnr;
+            }
+
+            // Verificar si el valor es numérico
+            if (trimmedKnnr.All(char.IsDigit))
+            {
+                // Formatear con ceros a la izquierda hasta 18 caracteres
+                string formattedKnnr = trimmedKnnr.PadLeft(knnrLength, '0');
+
+                return formattedKnnr;
+            }
+
+            // Si no es numérico ni comienza con letra, devolver tal cual con log de advertencia
+
+            return trimmedKnnr;
+        }
+
         [HttpGet("UpdateOrdenesProduccionController")]
 
         public async Task<IActionResult> GetCreateOrdenInversion(string I_ORDERID ="", string I_LIB = "", string I_CTEC = "", string I_REAP = "", string I_BLOQ = "", string I_DESBLOQ = "", string I_CERR = "", string I_BORR = "", string I_ANUL_BORR = "", string I_LOTE = "" ,string I_FECH_CIERRE="", string I_UARIS_MOD = "")
@@ -66,7 +100,7 @@ namespace WSpruebaArisSap.Controllers
                     I_UARIS_MOD = string.IsNullOrEmpty(I_UARIS_MOD) ? "" : I_UARIS_MOD;
                     var result = await context.CallFunction("ZPP_FM_ACT_ESTADO_OF",
                         Input: f => f
-                                        .SetField("I_ORDERID", I_ORDERID)
+                                        .SetField("I_ORDERID", FormatOrden(I_ORDERID))
                                         .SetField("I_LIB", I_LIB)
                                         .SetField("I_CTEC", I_CTEC)
                                         .SetField("I_REAP", I_REAP)
@@ -79,19 +113,49 @@ namespace WSpruebaArisSap.Controllers
                                          .SetField("I_FECH_CIERRE", string.IsNullOrWhiteSpace(I_FECH_CIERRE) ? new DateTime(1, 1, 1)  // equivalente a 0001-01-01
                                                                                                                                     : DateTime.ParseExact(I_FECH_CIERRE, "dd.MM.yyyy", null))
                                         .SetField("I_UARIS_MOD", I_UARIS_MOD),
-                        Output: f => f
-                            .MapTable("T_RETURN", s =>
-                                 from TYPE in s.GetField<string>("TYPE")    // CHAR
-                                 from ID in s.GetField<string>("ID")    // CHAR
-                                 from NUMBER in s.GetField<string>("NUMBER")    // CHAR
-                                 from MESSAGE in s.GetField<string>("MESSAGE")
-                                 select new
-                                 {
-                                     TYPE,
-                                     ID,
-                                     NUMBER,
-                                     MESSAGE
-                                 }));
+                         Output: f => (
+                        from E_STATUS in f.GetField<string>("E_STATUS")
+                        from E_ORDERID in f.GetField<string>("E_ORDERID")
+                        from T_RETURN in f.MapTable("T_RETURN", r =>
+                                    from TYPE in r.GetField<string>("TYPE")
+                                    from ID in r.GetField<string>("ID")
+                                    from NUMBER in r.GetField<string>("NUMBER")
+                                    from MESSAGE in r.GetField<string>("MESSAGE")
+                                    from LOG_NO in r.GetField<string>("LOG_NO")
+                                    from LOG_MSG_NO in r.GetField<decimal>("LOG_MSG_NO")
+                                    from MESSAGE_V1 in r.GetField<string>("MESSAGE_V1")
+                                    from MESSAGE_V2 in r.GetField<string>("MESSAGE_V2")
+                                    from MESSAGE_V3 in r.GetField<string>("MESSAGE_V3")
+                                    from MESSAGE_V4 in r.GetField<string>("MESSAGE_V4")
+                                    from PARAMETER in r.GetField<string>("PARAMETER")
+                                    from ROW in r.GetField<string>("ROW")
+                                    from FIELD in r.GetField<string>("FIELD")
+                                    from SYSTEM in r.GetField<string>("SYSTEM")
+                                    select new
+                                    {
+                                        TYPE,
+                                        ID,
+                                        NUMBER,
+                                        MESSAGE,
+                                        LOG_NO,
+                                        LOG_MSG_NO,
+                                        MESSAGE_V1,
+                                        MESSAGE_V2,
+                                        MESSAGE_V3,
+                                        MESSAGE_V4,
+                                        PARAMETER,
+                                        ROW,
+                                        FIELD,
+                                        SYSTEM
+                                    })
+                        select new
+                        {
+                            E_STATUS,
+                            E_ORDERID,
+                            T_RETURN
+                        }
+                    )
+                );
 
                     return Ok(new
                     {
