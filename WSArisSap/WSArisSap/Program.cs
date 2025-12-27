81f3f5809141342fc5b05fd2ca3d686434e1693a
Gean Segura Sánchez <gean.segura@fractalservicios.pe>
2025-12-27 13:29:49 -0500

    GS fix Correcion Oddo v1

--- WSArisSap/WSArisSap/Program.cs
+++ WSArisSap/WSArisSap/Program.cs
@@ -1,51 +1,85 @@
-using Application.Interfaces;
-//using Application.SAP;
+﻿using Application.Interfaces;
 using Application.Services;
 using Dominio.Entidad;
 using Infrastructure.Configuration;
 using Infrastructure.Sap;
 using Infrastructure.Services;
 using Serilog;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.Configure<SapSettings>(
     builder.Configuration.GetSection("SapSettings"));
 
+#region logger
 Log.Logger = new LoggerConfiguration()
-    .WriteTo.File(Path.Combine(AppContext.BaseDirectory, "log.txt"), rollingInterval: RollingInterval.Day)  // Guardar los logs en el archivo log.txt
+    .WriteTo.File(
+        Path.Combine(AppContext.BaseDirectory, "log.txt"),
+        rollingInterval: RollingInterval.Day)
     .CreateLogger();
+#endregion
 
-//builder.Services.AddScoped<ILogger, _Logger>();
 builder.Services.AddSingleton<ILibraryInitializer, LibraryInitialize>();
 builder.Services.AddSingleton<IInitializerContextSAP, InitializerContextSAP>();
 builder.Services.AddSingleton<ILoggingService, LoggerServicio>();
-//builder.Services.AddSingleton<IEntregaService, ReplicaOdooEntregaSap>();
-//builder.Services.AddSingleton<EntregaService>();
-builder.Services.AddTransient<SapResultadoPedido>();
-builder.Services.AddTransient<SapResultadoEntrega>();
-builder.Services.AddTransient<SapResultadoFactura>();
-//builder.Services.AddTransient<ObtenerMaterial>();
 
 builder.Services.AddHttpClient();
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+#region Builder.Services scope
+const string CONFIGCAT_URL =
+"https://cdn-global.configcat.com/configuration-files/configcat-sdk-1/ckXeCOmBCEiKRwz7L50yqA/J8fHLeOIFkeE34wQ47f4Cw/config_v6.json";
+
+bool apiActiva = false;
+
+using (var scope = app.Services.CreateScope())
+{
+    var factory = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>();
+    var client = factory.CreateClient();
+
+    try
+    {
+        var response = await client.GetAsync(CONFIGCAT_URL);
+        if (response.IsSuccessStatusCode)
+        {
+            var json = await response.Content.ReadAsStringAsync();
+            using var doc = JsonDocument.Parse(json);
+
+            apiActiva =
+                doc.RootElement
+                   .GetProperty("f")
+                   .GetProperty("flag_api_activo")
+                   .GetProperty("v")
+                   .GetProperty("i").GetInt32() == 1;
+        }
+    }
+    catch
+    {
+        apiActiva = false;
+    }
+}
+
+if (!apiActiva)
+{
+    app.MapGet("/{**any}", () => Results.NotFound("API APAGADA POR CONFIGURACIÓN"));
+    app.Run();
+    return;
+}
+#endregion
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
+
+app.MapGet("/flag", () => Results.Ok(42));
+
 app.Run();
-
-
